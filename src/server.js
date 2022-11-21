cb6cf7fac7bfca996da74733c0503705b9db2b35
cf9874 <cf9874@gmail.com>
2022-11-21 14:08:25 +0900

    GET method
    res.end() , res.send("string")

--- src/server.js
+++ src/server.js
@@ -3,6 +3,19 @@ import express from "express";
 const PORT = 4000;
 const app = express();
 
+const handleHome = (req, res) => {
+  console.log("hihi");
+
+  //   return res.end();
+  return res.send("I still love you.");
+};
+const handleLogin = (req, res) => {
+  return res.send("login page");
+};
+//how to respond GET request
+app.get("/", handleHome);
+app.get("/login", handleLogin);
+
 const handleListening = () => {
   console.log(`server listening on port http://localhost:${PORT}`);
 };
