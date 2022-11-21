41870d75e827c894d8f1a0879a6a8919cfb7f930
cf9874 <cf9874@gmail.com>
2022-11-21 18:41:55 +0900

    Using regular expression
    to protect route parameter (num)

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -3,12 +3,16 @@ import {
   watchVideo,
   editVideo,
   removeVideo,
+  searchVideo,
+  uploadVideo,
 } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-videoRouter.get("/watch", watchVideo);
-videoRouter.get("/edit", editVideo);
-videoRouter.get("/delete", removeVideo);
+videoRouter.get("/upload", uploadVideo);
+videoRouter.get("/:id(\\d+)", watchVideo);
+videoRouter.get("/:id(\\d+)/edit", editVideo);
+videoRouter.get("/:id(\\d+)/delete", removeVideo);
+videoRouter.get("/:id(\\d+)/search", searchVideo);
 
 export default videoRouter;
