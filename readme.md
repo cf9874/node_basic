d4a9baba7f5b1a43853866e42b85824ccbc8b18f
cf9874 <cf9874@gmail.com>
2022-11-21 16:18:32 +0900

    spec & Route basic

--- readme.md
+++ readme.md
@@ -1,4 +1,42 @@
+# SPEC
+
 - NodeJS
 - Express
 - Mongo
 - ES6
+
+# Function
+
+> ## 유저 계정 생성
+
+> ## 유저 로그인
+>
+> ## 유저 프로필 생성
+>
+> ## 유저 프로필 수정 / 삭제
+>
+> ## 유저 계정 정보 수정
+
+> ## 영상 업로드
+>
+> ## 영상 시청
+>
+> ## 업로드 된 영상 수정
+>
+> ## 자막 첨부
+
+# Route
+
+> / => Home
+> /join => Join
+> /login => Login
+> /search => Search
+
+> /users/edit => Edit profile
+> /users/delete => Delete profile
+
+> /videos/watch => Watch Video
+> /videos/edit => Edit Video
+> /videos/delete => Delete Video
+> /videos/comments => Comment on a Video
+> /videos/comments/delete => Delate A Comment of a Video
--- src/server.js
+++ src/server.js
@@ -4,23 +4,26 @@ import morgan from "morgan";
 const PORT = 4000;
 const app = express();
 const loggerMiddleware = morgan("dev");
-
-const handleHome = (req, res, next) => {
-  console.log("hihi");
-  return res.send("<h1>I still love you.</h1>");
-};
-const handleLogin = (req, res) => {
-  return res.send({ messge: "here is login page" });
-};
-
 app.use(loggerMiddleware);
 
-app.get("/", handleHome);
-app.get("/login", handleLogin);
+const globalRouter = express.Router();
+const handleHome = (req, res) => res.send("Home");
+globalRouter.get("/", handleHome);
+
+const userRouter = express.Router();
+const handleUser = (req, res) => res.send("Edit User");
+userRouter.get("/edit", handleUser);
+
+const videoRouter = express.Router();
+const handleVideo = (req, res) => res.send("Watch Video");
+videoRouter.get("/watch", handleVideo);
+
+app.use("/", globalRouter);
+app.use("/users", userRouter);
+app.use("/videos", videoRouter);
 
 const handleListening = () => {
   console.log(`server listening on port http://localhost:${PORT}`);
 };
-app.listen(PORT, handleListening);
 
-//port 설정
+app.listen(PORT, handleListening);
