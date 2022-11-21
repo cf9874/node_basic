1d70fe7544b03ff4c6a3f420ac332a2f2459a077
cf9874 <cf9874@gmail.com>
2022-11-21 15:43:59 +0900

    morgan
    Middleware library,
    options : dev, short, combined, common, tiny

--- src/server.js
+++ src/server.js
@@ -1,26 +1,21 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 const app = express();
-
-const testMiddleware = (req, res, next) => {
-  console.log(`you're in ${req.url} ㅋ`);
-  console.log("test middleware");
-
-  next();
-};
+const loggerMiddleware = morgan("dev");
 
 const handleHome = (req, res, next) => {
   console.log("hihi");
-
-  //   return res.end();
   return res.send("<h1>I still love you.</h1>");
 };
 const handleLogin = (req, res) => {
   return res.send({ messge: "here is login page" });
 };
-//how to respond GET request
-app.get("/", testMiddleware, handleHome);
+
+app.use(loggerMiddleware);
+
+app.get("/", handleHome);
 app.get("/login", handleLogin);
 
 const handleListening = () => {
