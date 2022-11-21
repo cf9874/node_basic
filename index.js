f56af400fa924f81df5d9a31f52be75510a061c8
cf9874 <cf9874@gmail.com>
2022-11-21 11:51:50 +0900

    babel

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("init");
+// const express = require("express");
+import express from "express";
+// const app = express();
+
+//babel is a js compiler
+//nodeJS는 js를 이해하지만 nodeJS가 이해하지 못하는 최신 JS 코드를
+//babel이 최신 js code를 구식 js 문법으로 변환해준다.
+// >> polyfill
+
+console.log("hi");
