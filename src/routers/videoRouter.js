336411cddb3f9fc95d648581165e427c16976995
cf9874 <cf9874@gmail.com>
2022-11-30 22:16:17 +0900

    #

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -3,8 +3,7 @@ import { deleteVideo, editVideo, uploadVideo, watchVideo } from "../controllers"
 
 export const videoRouter = express.Router();
 
+videoRouter.get("/upload", uploadVideo);
 videoRouter.get("/:id", watchVideo);
 videoRouter.get("/:id/edit", editVideo);
-videoRouter.get("/upload", uploadVideo);
-
 videoRouter.get("/:id/delete", deleteVideo);
