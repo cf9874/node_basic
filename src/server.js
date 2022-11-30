d27823690837e957b843e3c9e6a27fa55e69f86b
cf9874 <cf9874@gmail.com>
2022-11-30 14:43:32 +0900

    ~3.6 middleware

--- src/server.js
+++ src/server.js
@@ -3,13 +3,16 @@ import express from "express";
 const PORT = 4000;
 const app = express();
 
-const handleHome = (req, res, next) => {
-  res.send("Home");
+const handleHome = (req, res) => {
+  return res.send("Home");
+};
+
+const globalMiddleware = (req, res, next) => {
+  console.log(`someone is going to ${req.url}`);
   next();
 };
-// next 는 다음함수를 호출해줌
-
-app.get("/", handleHome, () => console.log("home"));
+app.use(globalMiddleware);
+app.get("/", handleHome);
 app.get("/users", () => console.log(9874));
 app.get("/videos", () => console.log(9598));
 
