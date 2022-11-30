5ea6196fbcf9d6e785365f9fdcfc6eb694a9c358
cf9874 <cf9874@gmail.com>
2022-11-30 14:09:02 +0900

    3.1~3.3 requset & response

--- src/server.js
+++ src/server.js
@@ -3,6 +3,12 @@ import express from "express";
 const PORT = 4000;
 const app = express();
 
+const handleHome = (req, res) => res.send("Home");
+
+app.get("/", handleHome);
+app.get("/users", () => console.log(9874));
+app.get("/videos", () => console.log(9598));
+
 const handleListening = () => {
   console.log(`server listening on port http://localhost:${PORT}`);
 };
